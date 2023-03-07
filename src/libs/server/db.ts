59c95d549fac198ee3fc9a7c418402b5d1301d3e
with-key <rem9238@kakao.com>
2023-03-08 07:51:59 +0900

    add: withHandler

--- src/libs/server/db.ts
+++ src/libs/server/db.ts
@@ -1,4 +1,10 @@
 import { PrismaClient } from "@prisma/client";
 
-const db = new PrismaClient();
+declare global {
+  var db: PrismaClient | undefined;
+}
+
+const db = global.db || new PrismaClient();
+if (process.env.NODE_ENV === "development") global.db = db;
+
 export default db;
