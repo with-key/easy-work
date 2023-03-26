c4419a640ae86948ddb4bb102e22e2b5935ab802
with-key <rem9238@kakao.com>
2023-03-27 01:25:51 +0900

    add: dayoff 메인 페이지

--- src/pages/home/dayoff/index.tsx
+++ src/pages/home/dayoff/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import { useGetDayoffList } from "@apis/repositories/dayoff";
+import DayoffList from "@features/dayoff/DayoffList";
 
 const DayoffMainPage = () => {
-  return <div>index</div>;
+  return (
+    <div>
+      <DayoffList></DayoffList>
+    </div>
+  );
 };
 
 export default DayoffMainPage;
