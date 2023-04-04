74134aebf304f766d5e50a7a0bf4e0d922a52dd5
with-key <rem9238@kakao.com>
2023-04-04 22:25:47 +0900

    fix: 오타 수정

--- src/styles/typos.ts
+++ src/styles/typos.ts
@@ -1,7 +1,7 @@
 type Typo = {
   fontWeight: number;
   fontSize: string;
-  lingHeight: string;
+  lineHeight: string;
   letterSpacing: string;
 };
 
@@ -11,23 +11,23 @@ export const genTypo: GenTypo = (fw, fs, lh, ls = -0.2) => {
   return {
     fontWeight: fw,
     fontSize: `${fs}px`,
-    lingHeight: `${lh}px`,
+    lineHeight: `${lh}px`,
     letterSpacing: `${ls}px`,
   };
 };
 
-export const typoes: { [key: string]: Typo } = {
-  "34-800": genTypo(800, 34, 35),
-  "26-800": genTypo(800, 26, 35),
-  "20-800": genTypo(800, 20, 30),
-  "17-700": genTypo(700, 17, 24),
-  "15-700": genTypo(700, 15, 23),
-  "15-600": genTypo(600, 15, 23),
-  "15-400": genTypo(400, 15, 23),
-  "14-700": genTypo(700, 14, 22),
-  "14-600": genTypo(600, 14, 22),
-  "14-400": genTypo(400, 14, 22),
-  "13-500": genTypo(500, 13, 22),
-  "13-400": genTypo(400, 13, 22),
-  "11-700": genTypo(700, 11, 14),
+export const typoes = {
+  T34_800: genTypo(800, 34, 35),
+  T26_800: genTypo(800, 26, 35),
+  T20_800: genTypo(800, 20, 30),
+  T17_700: genTypo(700, 17, 24),
+  T15_700: genTypo(700, 15, 23),
+  T15_600: genTypo(600, 15, 23),
+  T15_400: genTypo(400, 15, 23),
+  T14_700: genTypo(700, 14, 22),
+  T14_600: genTypo(600, 14, 22),
+  T14_400: genTypo(400, 14, 22),
+  T13_500: genTypo(500, 13, 22),
+  T13_400: genTypo(400, 13, 22),
+  T11_700: genTypo(700, 11, 14),
 };
